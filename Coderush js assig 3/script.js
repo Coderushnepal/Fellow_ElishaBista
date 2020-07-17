715a58d9f639a426f35209c414d3c16e8aac71b9
elisha408-max <60838279+elisha408-max@users.noreply.github.com>
2020-07-17 08:39:50 +0545

    Add files via upload
    Js assign ques 1 soln

---
+++ Coderush js assig 3/script.js
@@ -0,0 +1,17 @@
+function show(){
+    var pwd=document.getElementById("pwd");
+//    var icon=document.querySelector(".fa");
+  
+      if(pwd.type=="password"){
+          pwd.type="text";
+        
+
+      }
+      else{
+        pwd.type="password";
+      }
+    
+    };
+
+//   const icon=document.querySelector(".fa");
+//     console.log(icon);
