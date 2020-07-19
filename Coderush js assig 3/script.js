79ba24596a5f9ef92d74bb0efff96bc71721d7d9
elisha408-max <60838279+elisha408-max@users.noreply.github.com>
2020-07-19 06:39:50 +0545

    Add files via upload
    Assignment 3 completed

--- Coderush js assig 3/script.js
+++ Coderush js assig 3/script.js
@@ -1,14 +1,17 @@
 function show(){
     var pwd=document.getElementById("pwd");
+    var icon=document.getElementById("icon")
 //    var icon=document.querySelector(".fa");
   
       if(pwd.type=="password"){
           pwd.type="text";
+          icon.className="fa fa-eye";
         
 
       }
       else{
         pwd.type="password";
+        icon.className="fa fa-eye-slash";
       }
     
     };
