09f8bb5f0d06aa3b334896ecb9be4ed753682588
Elisha <elishabista538@gmail.com>
2020-07-26 12:52:45 +0545

    completed

--- JS/assig7/main.js
+++ JS/assig7/main.js
@@ -5,10 +5,9 @@ box.style.left="40%";
 box.style.top="20%";
 box.style.padding="20px";
 box.style.borderRadius="5px";
+box.style.boxShadow="2px 2px 2px 2px grey";
 
-
-box.style.backgroundColor="blue";
+// box.style.backgroundColor="blue";
 box.style.position="absolute";
-box.style.height="400px";
-box.style.width="400px";
+
 box.setAttribute("border","2px solid #000");
